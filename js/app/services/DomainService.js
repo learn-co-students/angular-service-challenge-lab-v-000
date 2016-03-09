e20fa181c2750d996460764a8a904452ac1b79b4
selichter <lichter.sarahe@gmail.com>
2016-03-08 20:42:45 -0600

    Done.

---
+++ js/app/services/DomainService.js
@@ -0,0 +1,11 @@
+function DomainService($http) {
+  this.getData = function(name) {
+    return $http.get('/rest/domain/' + name);
+  }
+
+}
+
+angular
+  .module('app')
+  .service('DomainService', DomainService);
+  
