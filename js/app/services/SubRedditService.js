0744dfa5c9703a76f31b91463ab015c6ae053dca
Chen Yang <yangc5@mail.uc.edu>
2016-03-19 18:41:43 +0000

    done

---
+++ js/app/services/SubRedditService.js
@@ -0,0 +1,8 @@
+function SubRedditService($http){
+  this.getData = function(name){
+    return $http.get('/rest/subreddit/'+name);
+  }
+}
+
+angular.module('app')
+       .service('SubRedditService', SubRedditService);
