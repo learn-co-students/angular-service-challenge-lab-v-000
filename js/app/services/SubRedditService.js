82e008b792514d51933a256f1342039a3849de61
Kevin McCormack <wittyman37@yahoo.com>
2016-03-21 15:48:56 -0400

    Done.

---
+++ js/app/services/SubRedditService.js
@@ -0,0 +1,10 @@
+function SubRedditService($http) {
+  this.getData = function (subRedditName) {
+    return $http.get('/rest/subreddit/' + subRedditName);
+  }
+
+}
+
+angular
+	.module('app')
+	.service('SubRedditService', SubRedditService);
