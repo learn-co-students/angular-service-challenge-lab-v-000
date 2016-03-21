27be8c9cfc22ce6d107445daae28e1aa5c4b4526
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-03-20 22:32:58 -0400

    Done

---
+++ js/app/services/DomainService.js
@@ -0,0 +1,11 @@
+function DomainService($http) {
+
+  this.getData = function(domainName) {
+    return $http.get('/rest/domain/' + domainName)
+  };
+
+}
+
+angular
+  .module('app')
+  .service('DomainService', DomainService)
