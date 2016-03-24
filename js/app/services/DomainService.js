fdd347965e7a53afa94056b293c28c74fd07254a
frankNowinski <nowinski.frank@gmail.com>
2016-03-24 03:44:03 -0400

    Done.

---
+++ js/app/services/DomainService.js
@@ -0,0 +1,9 @@
+function DomainService($http){
+  this.getData = function(name){
+    return $http.get('/rest/domain/' + name);
+  }
+}
+
+angular
+  .module('app')
+  .service('DomainService', DomainService);
