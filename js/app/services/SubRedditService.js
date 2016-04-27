6ad82319d6002378b224af88cb8e36f0b292e41c
NStephenson <nickstephenson90@gmail.com>
2016-04-26 23:58:59 -0400

    Done.

---
+++ js/app/services/SubRedditService.js
@@ -0,0 +1,11 @@
+function SubRedditService($http){
+
+  this.getData = function(subreddit){
+    return $http.get("/rest/subreddit/" + subreddit)
+  }
+
+}
+
+angular 
+  .module('app')
+  .service('SubRedditService', SubRedditService)
