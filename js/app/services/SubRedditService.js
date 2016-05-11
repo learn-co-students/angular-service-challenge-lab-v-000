27a1bea96a1ee6143553e840801df096ad8836d7
azheng249 <andy.zheng249@gmail.com>
2016-05-11 17:56:41 -0400

    Done.

---
+++ js/app/services/SubRedditService.js
@@ -0,0 +1,9 @@
+function SubRedditService ($http) {
+  this.getData = function (subredditName) {
+    return $http.get('/rest/subreddit/' + subredditName);
+  };
+}
+
+angular
+  .module('app')
+  .service('SubRedditService', SubRedditService);
