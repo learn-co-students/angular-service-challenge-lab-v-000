927cf56dfd246edfe4d622031dba67fc085a48de
Louiefigz <figueroa.aaron@gmail.com>
2016-06-05 13:47:25 -0400

    Done.

---
+++ js/app/services/SubRedditService.js
@@ -0,0 +1,10 @@
+function SubRedditService($http){
+
+  this.getData = function(url){
+    return $http.get('/rest/subreddit/' + url);
+  }
+}
+
+angular
+ .module('app')
+ .service('SubRedditService', SubRedditService);
