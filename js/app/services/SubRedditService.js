0e5e494b90a2eb752d045e01746f6d97d69ae230
the-widget <brennenawana108ny@gmail.com>
2016-06-21 15:14:24 -0400

    Done.

---
+++ js/app/services/SubRedditService.js
@@ -0,0 +1,11 @@
+function SubRedditService($http){
+  
+  this.getData = function(reddit){
+    return $http.get('/rest/subreddit/'+ reddit);
+  };
+
+};
+
+angular
+  .module('app')
+  .service('SubRedditService', SubRedditService)
