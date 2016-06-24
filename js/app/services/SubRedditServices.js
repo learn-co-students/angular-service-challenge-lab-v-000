2d87f0dd4f07ed5a6dfde98298af4dd81cd261a2
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-06-24 13:45:12 -0500

    Done.

---
+++ js/app/services/SubRedditServices.js
@@ -0,0 +1,9 @@
+var SubRedditService = function($http){
+  this.getData = function(subReddit){
+    return $http.get("/rest/subreddit/" + subReddit);
+  };
+}
+
+angular
+        .module("app")
+        .service("SubRedditService", SubRedditService);
