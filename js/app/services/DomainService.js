5f1a99fa29783e826d5667407486752be5db4f68
Smulligan85 <sean.mulligan85@gmail.com>
2016-08-05 10:39:41 -0700

    Add SubReddit and Domain Services

---
+++ js/app/services/DomainService.js
@@ -0,0 +1,10 @@
+function DomainService($http) {
+  this.getData = function(domainName) {
+    return $http.get('/rest/domain/' + domainName);
+  };
+}
+
+angular
+  .module('app')
+  .service('DomainService', DomainService);
+
