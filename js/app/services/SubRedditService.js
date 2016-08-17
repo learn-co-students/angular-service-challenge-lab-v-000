0012a29b17530250b6da3338685d9a9bb0b621eb
reichardn <reichardn@gmail.com>
2016-08-16 20:57:06 -0400

    Done.

---
+++ js/app/services/SubRedditService.js
@@ -0,0 +1,11 @@
+function SubRedditService($http) {
+
+  this.getData = function (data) {
+    var url = '/rest/subreddit/' + data;
+    return $http.get(url);
+  }
+}
+ 
+angular
+    .module('app')
+    .service('SubRedditService', SubRedditService);
