1506bbdf0b7439febd927a20d183fe1646bef23b
gabe <gj+code@mail.co.de>
2016-10-10 00:05:36 -0400

    Done.

---
+++ js/app/services/SubRedditService.js
@@ -0,0 +1,9 @@
+function SubRedditService($http) {
+  this.getData = function(subRedditName) {
+    return $http.get('/rest/subreddit/' + subRedditName);
+  };
+}
+
+angular
+  .module('app')
+  .service('SubRedditService', SubRedditService)
