5ec69dfb37a4254d6f5eb241cc10f740734e0707
ndaniele <n.daniele@gmail.com>
2016-11-05 14:56:01 -0400

    Done.

---
+++ js/app/services/SubRedditService.js
@@ -0,0 +1,10 @@
+function SubRedditService($http) {
+  this.getData = function(subreddit) {
+    return $http.get('/rest/subreddit/' + subreddit);
+  };
+}
+
+
+angular
+  .module('app')
+  .service('SubRedditService', SubRedditService);
