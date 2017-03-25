48e7be9fd9e4fce4a903c67c3a1535455a075763
Jon foster <redcabinny@gmail.com>
2017-03-25 17:41:37 -0400

    Done.

---
+++ js/app/services/SubRedditService.js
@@ -0,0 +1,12 @@
+function SubRedditService($http) {
+	this.getData = function(subreddit) {
+		var url = "/rest/subreddit/" + subreddit;
+		return $http.get(url)
+	}
+}
+
+
+
+angular
+	.module('app')
+	.service('SubRedditService', SubRedditService)
